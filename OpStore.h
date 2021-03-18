640d1ba0ffdaab38280c2611d05c69d51335de2c
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:05:58 -0600

    Update OpStore.h
    now inherits from MemoryOpCode

--- OpStore.h
+++ OpStore.h
@@ -3,9 +3,9 @@ OpStore.h and OpStore.cpp written by Tyler
 */
 #ifndef __OPSTORE__
 #define __OPSTORE__
-#include "OpCode.h"
+#include "MemoryOpCode.h"
 
-class OpStore : public OpCode
+class OpStore : public MemoryOpCode
 {
 public:
 	OpStore();//memory goes in the squiggly and in the constructor
