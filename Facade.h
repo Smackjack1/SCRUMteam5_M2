af3126141a274a3ba7ec81e1848cd9c7f5008b86
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 14:22:33 -0600

    Update Facade.h
    added two new commands to access opRead and opWrite commands

--- Facade.h
+++ Facade.h
@@ -3,12 +3,16 @@
 #include"Executor.h"
 #include"Memory.h"
 #include"Interpreter.h"
+#include"OpRead.h"
+#include"OpWrite.h"
 class Facade
 {
 private:
 	Executor myExecutor = Executor();
 	interpreter myInterpreter = interpreter();
 	vmMemory* myMemory = vmMemory::getInstance();
+	OpRead reader = OpRead();
+	OpWrite writer = OpWrite();
 public:
 	std::string getMemory();
 	void execute();
@@ -16,5 +20,8 @@ public:
 	void incrementPC(int increment);
 	std::string getCurrentOpCode();
 	int getCurrentOperand();
+	void opRead(std::string input); // give it the user input only, call it to execute opRead
+	std::string opWrite(); // executes and returns it's required output
+
 };
 #endif
