7fc67f1b87300c6913b87978ee0e5adbddc00c7c
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:02:33 -0600

    Update OpLoad.h
    now inherits from MemoryOpCode

--- OpLoad.h
+++ OpLoad.h
@@ -3,9 +3,9 @@ OpLoad.h and OpLoad.cpp written by Tyler
 */
 #ifndef __OPLOAD__
 #define __OPLOAD__
-#include "OpCode.h"
+#include "MemoryOpCode.h"
 
-class OpLoad : public OpCode
+class OpLoad : public MemoryOpCode
 {
 public:
 	OpLoad();//memory goes in the squiggly and in the constructor
