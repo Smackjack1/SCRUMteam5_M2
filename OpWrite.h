ae62806c92c471ec2a4825013f43a1924888ea65
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:10:14 -0600

    Update OpWrite.h

--- OpWrite.h
+++ OpWrite.h
@@ -9,7 +9,7 @@ class OpWrite : public GUIOpCode
 {
 public:
 	OpWrite();//memory goes in the squiggly and in the constructor
-	std::string execute(int argument, std::string input = ""); // input here can just be empty
+	std::string execute(int argument, std::string input); // input here should be ""
 };
 
 #endif
