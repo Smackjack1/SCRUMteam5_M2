acadc11ae1fc6d15ddf80f28f3e529b71a001486
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:00:32 -0600

    Update OpBranchZero.h
    now inherits from branchOpCode

--- OpBranchZero.h
+++ OpBranchZero.h
@@ -3,9 +3,9 @@ OpBranchZero.h and OpBranchZero.cpp written by Quinton
 */
 #ifndef __OPBRANCHZERO__
 #define __OPBRANCHZERO__
-#include "OpCode.h"
+#include "BranchOpCode.h"
 
-class OpBranchZero : public OpCode
+class OpBranchZero : public BranchOpCode
 {
 public:
 	OpBranchZero();//memory goes in the squiggly and in the constructor
