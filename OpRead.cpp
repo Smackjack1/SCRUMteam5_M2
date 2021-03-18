3d561712d2621100f4dfc075f48d566601a25425
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:12:51 -0600

    Update OpRead.cpp
    second parameter could not have its name redefined

--- OpRead.cpp
+++ OpRead.cpp
@@ -7,26 +7,25 @@ OpRead::OpRead()
 	this->code = "10";
 }
 
-std::string OpRead::execute(int arguement,std::string word)
+std::string OpRead::execute(int arguement,std::string input)
 {
 	unsigned int wordLength = 4;
 	int inputWord = 0;
 	char firstChar;
-	string word = "";
 	//cout << "Enter data as an integer (4 digits max):\n";//user prompt added by Kevin
 	//getline(cin,word);
-	firstChar = word.at(0);
+	firstChar = input.at(0);
 	if (firstChar == '-' or firstChar == '+')
 	{
 		wordLength = 5; // we can move our max length up by 1 to account for +/- if included
 	}
-	if (word.size() > wordLength)
+	if (input.size() > wordLength)
 	{
 		throw std::runtime_error("Incorrect argument for read command, 4 digits is the max");
 	}
 	try 
 	{
-		inputWord = stoi(word);
+		inputWord = stoi(input);
 	}
 	catch(std::invalid_argument) // only want to catch invalid argument right here
 	{
