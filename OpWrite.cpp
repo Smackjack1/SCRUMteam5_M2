a3f6a14ba1d9d824b19b18edf0ede877823292dc
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 09:08:30 -0600

    Update OpWrite.cpp
    execute now returns it's output instead of printing to console

--- OpWrite.cpp
+++ OpWrite.cpp
@@ -7,9 +7,10 @@ OpWrite::OpWrite()
 	this->code = "11";
 }
 
-void OpWrite::execute(int argument)
+std::string OpWrite::execute(int argument, std::string input = "")
 {
 	int word;
 	this->memory->read(argument, &word);
-	cout << "The word at location " << argument << " was " << word << endl;
+	return "The word at location " + std::to_string(argument) + " was " + std::to_string(word) + "\n";
+	//cout << "The word at location " << argument << " was " << word << endl;
 }
