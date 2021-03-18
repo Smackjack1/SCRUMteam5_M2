1a8f86668256d7edca12c69366a6f2c8639919c2
Queueue-ux <70137026+Queueue-ux@users.noreply.github.com>
2021-03-18 08:56:21 -0600

    Update Facade.cpp
    added a way to increment PC, access opcode, and access operand from memory

--- Facade.cpp
+++ Facade.cpp
@@ -14,3 +14,21 @@ bool Facade::addCommand(std::string command)
 {
     return myInterpreter.parseCommand(command);
 }
+
+void Facade::incrementPC(int increment = 1)
+{
+    int currentPC = 0;
+    myMemory->read(PC, &currentPC);
+    currentPC += increment; // fixed by Quinton, was calling &mainmem which cannot be done, now it has a local variable that stores pc value, then increments by 1 and writes new value to PC
+    myMemory->write(PC, &currentPC);
+}
+
+std::string Facade::getCurrentOpCode()
+{
+    return myExecutor.GetOpcode();
+}
+
+int Facade::getCurrentOperand()
+{
+    return myExecutor.GetOperand();
+}
